12911a8c2aa739295e326d48ffbf9a1de09f6b24
JohnPaul-Londono <johnplondono@gmail.com>
2022-01-18 20:31:17 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import { useEffect, useState } from 'react';
+import axios from 'axios';
 
 function App() {
+  const [pokemon, setPokemon] = useState([]);
+  const [name, setName] = useState("");
+
+  useEffect(() => {
+    axios.get("https://pokeapi.co/api/v2/pokemon?limit=898")
+      .then(res => {
+        console.log(res);
+        setPokemon(res.data.results);
+      })
+      .catch(err => console.log(err))
+  }, [name])
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Pokemon API Using Fetch</h1>
+      <form onSubmit={onSubmitHandler}>
+        <button >Fetch Pokemon</button>
+      </form>
+
+
+      <ul>
+        {
+          pokemon.map((pokemon, i) => {
+            return <li key={i}><a href={pokemon.url}>{pokemon.name}</a></li>
+          })
+        }
+      </ul>
     </div>
   );
 }
